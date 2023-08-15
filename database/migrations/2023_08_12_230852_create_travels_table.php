53826122f36ccede8e8be639087402ca4e7d1135
Aleksandr-code <70668106+Aleksandr-code@users.noreply.github.com>
2023-08-15 23:01:39 +0600

    added models and migrations

--- database/migrations/2023_08_12_230852_create_travels_table.php
+++ database/migrations/2023_08_12_230852_create_travels_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->uuidMorphs('tokenable');
+        Schema::create('travels', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->boolean('is_public')->default(false);
+            $table->string('slug')->unique();
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->text('description');
+            $table->unsignedInteger('number_of_days');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('tralevs');
     }
 };
